27196d3e6962883e5f54fc535d640eada91a1321
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2010-06-04 11:12:38 +0200

    added a mostly empty module

---
+++ plugins/python-testing.el
@@ -0,0 +1,9 @@
+;; Snippets to handle the testing of python modules
+;; 
+;; Ideas: 
+;; - executing preserving the directory
+
+(defun python-exec (command)
+  "The following function launches a command. When recalling the
+  function you can type just RET and the command is launched with
+  the same working directory as before" )
