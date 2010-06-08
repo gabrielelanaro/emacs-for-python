21fa76b24a00b9248c667ba2e0c02b7051f3fe6a
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2010-06-08 18:00:50 +0200

    cleaned a bit the stuff

---
+++ plugins/pyflakeswrap.py
@@ -0,0 +1,8 @@
+import subprocess,shlex
+import sys
+import os
+cmd = shlex.split("pyflakes "+sys.argv[1])
+out = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+stdout,stderr = out.communicate()
+msg = ":".join(stdout.split(":")[:-1])+":Warning:" + stdout.split(":")[-1]
+print msg
