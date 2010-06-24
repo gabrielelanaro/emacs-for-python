344118a68736118d7c78a9bce71c44dd2b0295b6
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2010-06-24 21:41:54 +0200

    Fixed bug #13 added auto open rope project

--- epy-init.el
+++ epy-init.el
@@ -27,9 +27,6 @@
 ;; Extensions 
 ;;============
 
-;(setq pymacs-available
-; (require 'pymacs "pymacs" t))
-
 ;; Yasnippet
 (require 'yasnippet)
 
@@ -69,6 +66,16 @@
   (add-hook 'rope-open-project-hook 'ac-nropemacs-setup)
   (setq ropemacs-guess-project t)
   (setq ropemacs-enable-autoimport t)
+
+  ;; Adding hook to automatically open a rope project if there is one
+  ;; in the current or in the upper level directory
+  (add-hook 'python-mode-hook 
+	    (lambda () 
+	      (cond ((file-exists-p ".ropeproject")
+		     (rope-open-project default-directory))
+		    ((file-exists-p "../.ropeproject")
+		     (rope-open-project (concat default-directory "..")))
+		    )))
   )
 
 ;; ibuffer by default
