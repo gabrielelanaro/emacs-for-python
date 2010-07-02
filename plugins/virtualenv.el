e80e8e6cb1a56867a9ddc3af05b0996e29da855f
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2010-07-02 17:02:55 +0200

    workin on virtualenv ext

--- plugins/virtualenv.el
+++ plugins/virtualenv.el
@@ -1,8 +1,34 @@
-;; This module defines the workon command for using virtualenv inside
-;; emacs
+;;; virtualenv.el --- Switching virtual python enviroments seamlessly
+
+;; Copyright (C) 2010 Gabriele Lanaro
+
+;; Author: Gabriele Lanaro <gabriele.lanaro@gmail.com>
+;; Version: 0.1
+;; Url: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with EMMS; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; 
+
+
 (setq workon-home (getenv "WORKON_HOME"))
 
-(defun add-to-PATH (dir)
+(defun virtualenv-add-to-path (dir)
   "Add the specified path element to the Emacs PATH"
   (interactive "DEnter directory to be added to PATH: ")
   (if (file-directory-p dir)
@@ -11,10 +37,23 @@
                       path-separator
                       (getenv "PATH")))))
 
-(defun activate-virtualenv (dir)
+(defun virtualenv-name-buffer (buffer)
+  "Adds the virtualenv name to the buffer, as like in the prompt"
+  (with-current-buffer buffer
+    (unless (string-match (buffer-name) 
+			  (concat "^(" (buffer-local-value 'virtualenv-name) ")"))
+      
+      (rename-buffer (concat "(" (buffer-local-value 'virtualenv-name) ")"))
+      )
+    )
+  )
+
+(defun virtualenv-activate (dir)
   (setenv "VIRTUAL_ENV" dir)
-  (add-to-PATH (concat dir "/bin"))
-  (add-to-list 'exec-path (concat dir "/bin")))
+  (virtualenv-add-to-path (concat dir "/bin"))
+  (add-to-list 'exec-path (concat dir "/bin"))
+  
+  )
 
 (defun is_virtualenv (dir)
   "Check if a directory is a virtualenv"
@@ -41,7 +80,7 @@
 (defun workon (name)
   "Issue a virtualenvwrapper-like workon command"
   (interactive (list (completing-read "Virtualenv: " (workon-complete))))
-  (activate-virtualenv (concat (getenv "WORKON_HOME") "/" name))
+  (virtualenv-activate (concat (getenv "WORKON_HOME") "/" name))
   )
 
 
