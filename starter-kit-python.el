5d2641fdae3734f02f32453168c6e6614c3bb299
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2010-08-01 21:23:07 +0200

    Further cleaning, splitting python related stuff from the rest

--- starter-kit-python.el
+++ starter-kit-python.el
@@ -30,61 +30,19 @@
 ;; Yasnippet
 (autoload 'yas/initialize "yasnippet" "Start Yasnippet" t)
 (eval-after-load 'yasnippet
-  ('yas/initialize)
+  (yas/initialize)
   (yas/load-directory (concat epy-install-dir "yasnippet/snippets"))
   (setq yas/prompt-functions '(yas/ido-prompt yas/dropdown-prompt))
   )
 
 ;; Auto-completion
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories 
-	     (concat epy-install-dir "auto-complete/ac-dict"))
-(ac-config-default)
-(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
-
-;; Rope, this one is more contrived, we have to check if we have
-;; pymacs.
-
-;; First adding to python path custom rope extensions
-(setenv "PYTHONPATH"
-	(concat 
-	 (getenv "PYTHONPATH") ":"
-	 (concat epy-install-dir "rope-dist")))
-
-(when (require 'pymacs) 
-  (setq pymacs-load-path 
-	(list 
-	 (concat epy-install-dir "rope-dist/ropemacs/")))
-  (pymacs-load "ropemacs" "rope-")
-  (load (concat epy-install-dir "completion/ac-ropemacs-config.el"))
-  
-  ;; Pretty custom, I've patched ropemode and ropemacs to add this
-  ;; hook.
-  ;;
-  ;; There is also a custom hook to find if there is a project and if
-  ;; there is activate it. In this way the project is automatically opened.
-  (add-hook 'rope-open-project-hook 'ac-nropemacs-setup)
-  (setq ropemacs-guess-project t)
-  (setq ropemacs-enable-autoimport t)
-  (setq ropemacs-codeassist-maxfixes 3)
-  ;; Adding hook to automatically open a rope project if there is one
-  ;; in the current or in the upper level directory
-  (add-hook 'python-mode-hook 
-	    (lambda () 
-	      (cond ((file-exists-p ".ropeproject")
-		     (rope-open-project default-directory))
-		    ((file-exists-p "../.ropeproject")
-		     (rope-open-project (concat default-directory "..")))
-		    )))
+(autoload ac-config-default "auto-complete-config")
+(eval-after-load 'auto-complete-config
+  (ac-config-default)
+  (add-to-list 'ac-dictionary-directories 
+               (concat epy-install-dir "auto-complete/ac-dict"))
+  (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
   )
-
-;; ibuffer by default
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-
-;; Ido mode with fuzzy matching
-(require 'ido)
-(ido-mode t)
-(setq ido-enable-flex-matching t) ;; enable fuzzy matching
 
 ;; Parentheses Pairing
 (setq skeleton-pair t)
@@ -94,26 +52,6 @@
 (global-set-key "{" 'skeleton-pair-insert-maybe)
 (global-set-key "\"" 'skeleton-pair-insert-maybe)
 
-;; Open Next Line
-(require 'open-next-line)
-
-;; Eshell tweaks
-;; Visual commands like ipython
-(add-hook
- 'eshell-mode-hook
-  (lambda ()
-    (setq
-     eshell-visual-commands
-     (append
-      '("mutt" 
-	"vim" 
-	"screen" 
-	"lftp" 
-	"ipython" 
-	"telnet"
-	"ssh")
-       eshell-visual-commands))))
-
 ;; Virtualenv workon command
 (require 'virtualenv)
 
@@ -121,19 +59,9 @@
 (require 'python-flymake)
 
 (require 'smart-operator)
+
 ;;=====================
 ;; Keybindings Section
 ;;=====================
-
-;; Copy-Cut-Paste from clipboard with Super-C Super-X Super-V
-(global-set-key (kbd "s-x") 'clipboard-kill-region) ;;cut
-(global-set-key (kbd "s-c") 'clipboard-kill-ring-save) ;;copy
-(global-set-key (kbd "s-v") 'clipboard-yank) ;;paste
-
-;; calc-mode more comfortable
-(global-set-key (kbd "M-c") 'calc-dispatch)
-
-; Ctrl+tab mapped to Alt+tab
-(define-key function-key-map [(control tab)] [?\M-\t])
 
 (provide 'epy-init)
