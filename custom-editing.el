39710fcb254f151a341b6479b01ea0e76111bca9
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2010-08-02 15:55:04 +0200

    New file to split the various customizations

---
+++ custom-editing.el
@@ -0,0 +1,28 @@
+
+
+(require 'smart-operator)
+;; Parentheses Pairing
+(setq skeleton-pair t)
+
+(global-set-key "(" 'skeleton-pair-insert-maybe)
+(global-set-key "[" 'skeleton-pair-insert-maybe)
+(global-set-key "{" 'skeleton-pair-insert-maybe)
+(global-set-key "\"" 'skeleton-pair-insert-maybe)
+
+
+;; Open Next Line
+(require 'open-next-line)
+
+;; Auto Completion
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories 
+	     (concat epy-install-dir "auto-complete/ac-dict"))
+(ac-config-default)
+
+;; Yasnippet
+(require 'yasnippet)
+
+(yas/initialize)
+(yas/load-directory (concat epy-install-dir "yasnippet/snippets"))
+
+(setq yas/prompt-functions '(yas/ido-prompt yas/dropdown-prompt))
