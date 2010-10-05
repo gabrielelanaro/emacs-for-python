f97f98806e33fbbe9665b2729ddd31c333c5319a
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2010-10-05 11:13:32 +0200

    Organized all the loading code in another package, epy-setup.el

--- epy-init.el
+++ epy-init.el
@@ -1,29 +1,5 @@
 ;; This file initializate all the extensions contained in this package
 
-;; Trick to get the filename of the installation directory
-(defconst epy-install-dir
-  (file-name-directory (or load-file-name
-                           (when (boundp 'bytecomp-filename) bytecomp-filename)
-                           buffer-file-name))
-  "Installation directory of emacs-for-python"
-)
-
-;;
-;; Adjust load path to add the following paths
-;; yasnippet/
-;; plugins/
-;; auto-complete
-
-(add-to-list 'load-path
-             epy-install-dir)
-(add-to-list 'load-path
-	     (concat epy-install-dir "yasnippet"))
-(add-to-list 'load-path
-	     (concat epy-install-dir "plugins"))
-(add-to-list 'load-path
-	     (concat epy-install-dir "auto-complete"))
-(add-to-list 'load-path
-	     (concat epy-install-dir "flymake"))
 
 (require 'starter-kit-python)
 (require 'starter-kit-completion)
---
+++ epy-setup.el
@@ -0,0 +1,27 @@
+;; epy-setup.el - setup and load all the paths where the extensions
+;; are contained
+
+;; Trick to get the filename of the installation directory
+(defconst epy-install-dir
+  (file-name-directory (or load-file-name
+                           (when (boundp 'bytecomp-filename) bytecomp-filename)
+                           buffer-file-name))
+  "Installation directory of emacs-for-python"
+)
+
+;;
+;; Adjust load path to add the following paths
+;; yasnippet/
+;; plugins/
+;; auto-complete
+
+(add-to-list 'load-path
+             epy-install-dir)
+(add-to-list 'load-path
+	     (concat epy-install-dir "yasnippet"))
+(add-to-list 'load-path
+	     (concat epy-install-dir "plugins"))
+(add-to-list 'load-path
+	     (concat epy-install-dir "auto-complete"))
+(add-to-list 'load-path
+	     (concat epy-install-dir "flymake"))
