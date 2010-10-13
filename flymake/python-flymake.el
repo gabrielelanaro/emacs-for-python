7c2df4dbe00963d2f1c0f5c1d0dfd2c7423269ff
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2010-10-13 22:41:14 +0200

    Modified the python-flymake configuration to work with pylint

--- flymake/python-flymake.el
+++ flymake/python-flymake.el
@@ -1,4 +1,5 @@
 ;; Flymake configuration with python
+;; TODO: Refactorings!
 (require 'tramp)
 
 (when (load "flymake-patch" t) 
@@ -16,7 +17,7 @@
 
   (defun flymake-pep8-init () 
     ;; Pyflakes stuff
-     ; Make sure it's not a remote buffer or flymake would not work
+    ;; Make sure it's not a remote buffer or flymake would not work
      (when (not (subsetp (list (current-buffer)) (tramp-list-remote-buffers)))
       (let* ((temp-file (flymake-init-create-temp-buffer-copy 
                          'flymake-create-temp-inplace)) 
@@ -27,7 +28,28 @@
 
 
   (add-to-list 'flymake-allowed-file-name-masks 
-               '("\\.py\\'" flymake-pyflakes-init)))
+               '("\\.py\\'" flymake-pyflakes-init))
+
+
+  ;; We need additional scripts!! TODO: Moving this in an appropriate
+  ;; place like epy-setup.el
+  
+  (add-to-list 'exec-path (concat epy-install-dir "scripts"))
+  (defun flymake-pylint-init () 
+    ;; Pyflakes stuff
+    ;; Make sure it's not a remote buffer or flymake would not work
+    (when (not (subsetp (list (current-buffer)) (tramp-list-remote-buffers)))
+      (let* ((temp-file (flymake-init-create-temp-buffer-copy 
+                         'flymake-create-temp-inplace)) 
+             (local-file (file-relative-name 
+                          temp-file 
+                          (file-name-directory buffer-file-name)))) 
+        (list "python" (list (concat epy-install-dir "scripts/pylint-mod.py") local-file)))))
+  
+  
+  (add-to-list 'flymake-allowed-file-name-masks 
+               '("\\.py\\'" flymake-pylint-init))
+  )
 
 ;; Adding to the variable the regexps that matches warning messages
 ;;(setq flymake-log-level 3)
