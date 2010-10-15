1e9df3937ef8afb58ea053cbaa406999f3ac1c00
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2010-10-15 16:43:21 +0200

    The epy-setup needed the directory path epy-install-dir to work

--- epy-setup.el
+++ epy-setup.el
@@ -6,6 +6,12 @@
 ;; yasnippet/
 ;; plugins/
 ;; auto-complete
+(defconst epy-install-dir
+  (file-name-directory (or load-file-name
+                           (when (boundp 'bytecomp-filename) bytecomp-filename)
+                           buffer-file-name))
+  "Installation directory of emacs-for-python"
+)
 
 (add-to-list 'load-path
              epy-install-dir)
