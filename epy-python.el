68806aa845ed5703e53acedcc1b8a8f321fc38dc
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2011-03-02 22:17:58 +0100

    new closure-based flymake configuration

--- epy-python.el
+++ epy-python.el
@@ -25,10 +25,6 @@
                     )))
   )
 
-;; Customizing
-(setq flymake-enable-pyflakes t)
-(setq flymake-enable-pylint nil)
-(setq flymake-enable-pep8 nil)
 
 ;; Python or python mode?
 (eval-after-load 'python
@@ -51,20 +47,10 @@
      ;; Flymake for python configuration
      ;;===================================================
      
-     ;; TODO: There is some duplication, that can be removed using macros
-     ;; TODO: Implement flymake-remove-checker
-
      ;; Instructions to add a new checker based on command:
      ;;
-     ;; 1) Write an init function, the flymake-command-setup performs some
-     ;;    checks and at the end of the option list the filename to process:
+     ;; (flymake-setup-command "command" (list "option1" "option2")))
      ;;
-     ;;   (defun flymake-newchecker-init ()
-     ;;      (flymake-command-setup "command" (list "option1" "option2")))
-     ;;
-     ;; 2) Use the flymake-add-checker function
-     ;;
-     ;;    (flymake-add-checker flymake-newchecker-init)
 
      (require 'tramp)
      ;; Utilities that increase legibility and reduce code duplication
@@ -78,56 +64,35 @@
                           'flymake-create-temp-inplace)))
          (file-relative-name 
           temp-file 
-          (file-name-directory buffer-file-name))))
+          (file-name-directory buffer-file-name))))     
 
-     (defun flymake-command-setup (command &optional options)
-       "Setup the command to be used with flymake, the command
-will be called in this way: COMMAND OPTIONS FILE The FILE varible
-is passed after the options."
-       ;; Make sure it's not a remote buffer or flymake would not work
-       (when (not (current-file-remotep)) 
-         (list command
-               (append options (list (flymake-create-copy-file))))))
 
      (when (require 'flymake "flymake-patch" t)
        (setq flymake-info-line-regex
              (append flymake-info-line-regex '("unused$" "^redefinition" "used$"))))
-
-     ;; I'm using individual well-defined names to be able to remove them
-     ;; in some way
-
-     ;; Init functions!
-     (defun flymake-pyflakes-init ()
-       (flymake-command-setup "pyflakes"))
-
-     (defun flymake-pep8-init ()
-       (flymake-command-setup "pep8"))
-
-     (defun flymake-pylint-init ()
-       (flymake-command-setup "python" (list (concat epy-install-dir "scripts/pylint-mod.py"))))
-
-     (defun flymake-disable-python-checkers ()
-       "Disable all python checkers"
-       (dolist (flymake-checker-init '(flymake-pyflakes-init flymake-pep8-init flymake-pylint-init))
-         (remove '("\\.py\\'" flymake-checker-init) 'flymake-allowed-file-name-masks)))
-
-     (defun flymake-add-checker (command)
-       "Add the checker specified by the COMMAND list"
+     
+     (defun flymake-setup-command (command options pattern)
+       "Setup a new checker only by specifying its command, options and pattern to apply"
        (add-to-list 'flymake-allowed-file-name-masks
-                    (list "\\.py\\'" command)))
-
+                    (list pattern
+                          ;; Define the init function, actually a closure
+                          `(lambda ()
+                             ;; Make sure it's not a remote buffer or flymake would not work
+                             (when (not (current-file-remotep)) 
+                               (list ,command
+                                     (append ,options (list (flymake-create-copy-file)))))
+                             )))
+       )
+     
+     (defun epy-setup-checker (command &optional options)
+       "Setup specifically a python checker"
+       (flymake-setup-command command options "\\.py\\'"))
+     
+     (defun flymake-setup-multiple-commands (commandlst pattern)
+       "TODO Multiple checker on the same python buffers")
      ;; Not on all modes, please
      (add-hook 'python-mode-hook 'flymake-find-file-hook)
-
-     
-     (when flymake-enable-pyflakes
-       (flymake-add-checker 'flymake-pyflakes-init))
-
-     (when flymake-enable-pylint
-       (flymake-add-checker 'flymake-pylint-init))
-
-     (when flymake-enable-pep8
-       (flymake-add-checker 'flymake-pep8-init)))
+     )
   )
 ;; Cython Mode
 (autoload 'cython-mode "cython-mode" "Mode for editing Cython source files")
