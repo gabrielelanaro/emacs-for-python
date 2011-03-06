db3f77d60f3f7a480c1bf9a59dc6505845baad44
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2011-03-06 23:01:33 +0100

    Completion updated to handle snippets

--- epy-completion.el
+++ epy-completion.el
@@ -16,8 +16,10 @@
 
 ;; Live completion with auto-complete
 ;; (see http://cx4a.org/software/auto-complete/)
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories (concat epy-install-dir "extensions/auto-complete/dict/"))
+(require 'auto-complete-config nil t)
+(add-to-list 'ac-dictionary-directories (concat dotfiles-dir "elpa-to-submit/auto-complete/dict/"))
+;; Do What I Mean mode
+(setq ac-dwim t)
 (ac-config-default)
 
 ;; set also the completion for eshell
@@ -27,6 +29,28 @@
 (define-key ac-complete-mode-map "\r" 'ac-complete)
 (define-key ac-complete-mode-map "\M-n" 'ac-next)
 (define-key ac-complete-mode-map "\M-p" 'ac-previous)
+
+
+;; Disabling Yasnippet completion 
+(defun epy-snips-from-table (table)
+  (with-no-warnings
+    (let ((hashtab (ac-yasnippet-table-hash table))
+          (parent (ac-yasnippet-table-parent table))
+          candidates)
+      (maphash (lambda (key value)
+                 (push key candidates))
+               hashtab)
+      (identity candidates)
+      )))
+
+(defun epy-get-all-snips ()
+  (let (candidates)
+    (maphash
+     (lambda (kk vv) (push (epy-snips-from-table vv) candidates)) yas/tables)
+    (apply 'append candidates))
+  )
+
+(setq ac-ignores (concatenate 'list ac-ignores (epy-get-all-snips)))
 
 ;; ropemacs Integration with auto-completion
 (defun ac-ropemacs-candidates ()
