70686a5dc004160c56028857bd29b7bd172ad5e7
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2011-03-07 21:41:11 +0100

    packaging fixed and with new features

--- scripts/package.py
+++ scripts/package.py
@@ -1,28 +1,38 @@
 import glob
 import itertools
-
-from scriptine import run
-
-from scripting.commands import rm,cp,mkdir,sh_cmdln,archive,find
-
+import os
+from scripting.commands import rm,cp,mkdir,sh_cmdln, sh_args, archive,find
+from scripting.runner import command, run
 
 VERSION = "0.2"
-def compile_el_file(fn):
+def compile_el(fn, load='epy-init.el'):
     """
-    Compile an emacs file FN
+    Compile a set of .el files using emacs
     """
-    sh_cmdln("emacs -Q -l epy-init.el -batch -f batch-byte-compile",[fn])
+    cmdlist = ['emacs', '-Q']
+    if load:
+        cmdlist.append(load)
+    cmdlist += ['-batch', '-f', 'batch-byte-compile']
+    cmdlist.extend(fn)
+    sh_args(cmdlist)
 
+@command
+def compile():
+    compile_el(find('*.el'))
+
+@command
 def clean():
     """TODO: Clean the build stuff etc...
     """
     # removing compiled files
-    [rm(f) for f in find("*.pyc")]
-    [rm(f) for f in find("*.elc")]
+    rm(find("*.pyc"))
+    rm(find('*.elc'))
     
-    rm("dist")
+    if os.path.exists("dist"):
+        rm("dist")
 
-def package_command():
+@command
+def package(VERSION):
     """Package the emacs-for-python distribution
     """
     clean()
@@ -32,9 +42,13 @@ def package_command():
     # Concatenating lists
     FILES = itertools.chain(*(glob.glob(g) for g in GLOBS))
     mkdir("dist/emacs-for-python-"+VERSION,parent=True)
-    [cp(f,"dist/emacs-for-python-"+VERSION) for f in FILES]
-    archive(["dist/emacs-for-python-"+VERSION], 
+    cp(FILES,"dist/emacs-for-python-"+VERSION)
+    archive("dist/emacs-for-python-"+VERSION, 
             "dist/emacs-for-python-%s.tar.gz"%VERSION,format="gzip")
+
+@command
+def test_run():
+    sh_cmdln('emacs -Q -l epy-init.el', [])
 
 
 if __name__ == '__main__':
