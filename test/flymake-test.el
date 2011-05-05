ecd5477f7908533fa0aa79a8a23639258fe1476c
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2011-05-05 23:22:19 +0200

    test file, brainstorming on how flymake configuration should behave

---
+++ test/flymake-test.el
@@ -0,0 +1,8 @@
+
+;; how to implement ccheckers, ala windows
+;; %f means the abs filename
+;; %d may be the directory
+;; Implement in this way: use replace-regexp-in-string and then
+;; shlex-split, then pass to flymake
+(epy-setup-checker "flymake %f")
+(epy-setup-checker "python mycustom.py %f")
