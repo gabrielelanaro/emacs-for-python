c51886cc08897bfd35b17375434a0ed39376934f
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2011-06-15 22:01:08 +0200

    reordered and cleaned the changes done by Basti

--- epy-bindings.el
+++ epy-bindings.el
@@ -8,4 +8,8 @@
 
 ; Ctrl+tab mapped to Alt+tab
 (define-key function-key-map [(control tab)] [?\M-\t])
- (provide 'epy-bindings)
+
+(global-set-key [f10] 'flymake-goto-prev-error)
+(global-set-key [f11] 'flymake-goto-next-error)
+
+(provide 'epy-bindings)
--- epy-editing.el
+++ epy-editing.el
@@ -36,4 +36,100 @@
 ;; Eproject project management with emacs
 (require 'eproject)
 
+;; code borrowed from http://emacs-fu.blogspot.com/2010/01/duplicating-lines-and-commenting-them.html
+(defun djcb-duplicate-line (&optional commentfirst)
+  "comment line at point; if COMMENTFIRST is non-nil, comment the
+original" (interactive)
+  (beginning-of-line)
+  (push-mark)
+  (end-of-line)
+  (let ((str (buffer-substring (region-beginning) (region-end))))
+    (when commentfirst
+    (comment-region (region-beginning) (region-end)))
+    (insert-string
+      (concat (if (= 0 (forward-line 1)) "" "\n") str "\n"))
+    (forward-line -1)))
+
+;; duplicate a line
+(global-set-key (kbd "C-c y") 'djcb-duplicate-line)
+
+;; duplicate a line and comment the first
+(global-set-key (kbd "C-c c")
+(lambda()(interactive)(djcb-duplicate-line t)))
+
+; code copied from http://stackoverflow.com/questions/2423834/move-line-region-up-and-down-in-emacs
+(defun move-text-internal (arg)
+  (cond
+   ((and mark-active transient-mark-mode)
+    (if (> (point) (mark))
+        (exchange-point-and-mark))
+    (let ((column (current-column))
+          (text (delete-and-extract-region (point) (mark))))
+      (forward-line arg)
+      (move-to-column column t)
+      (set-mark (point))
+      (insert text)
+      (exchange-point-and-mark)
+      (setq deactivate-mark nil)))
+   (t
+    (let ((column (current-column)))
+      (beginning-of-line)
+      (when (or (> arg 0) (not (bobp)))
+        (forward-line)
+        (when (or (< arg 0) (not (eobp)))
+          (transpose-lines arg))
+        (forward-line -1))
+      (move-to-column column t)))))
+
+(defun move-text-down (arg)
+  "Move region (transient-mark-mode active) or current line
+  arg lines down."
+  (interactive "*p")
+  (move-text-internal arg))
+
+(defun move-text-up (arg)
+  "Move region (transient-mark-mode active) or current line
+  arg lines up."
+  (interactive "*p")
+  (move-text-internal (- arg)))
+
+; patches by balle
+; http://www.datenterrorist.de
+(defun balle-python-shift-left ()
+  (interactive)
+  (let (start end bds)
+    (if (and transient-mark-mode
+	   mark-active)
+	(setq start (region-beginning) end (region-end))
+      (progn
+	(setq bds (bounds-of-thing-at-point 'line))
+	(setq start (car bds) end (cdr bds))))
+  (python-indent-shift-left start end))
+  (setq deactivate-mark nil)
+)
+
+(defun balle-python-shift-right ()
+  (interactive)
+  (let (start end bds)
+    (if (and transient-mark-mode
+	   mark-active)
+	(setq start (region-beginning) end (region-end))
+      (progn
+	(setq bds (bounds-of-thing-at-point 'line))
+	(setq start (car bds) end (cdr bds))))
+  (python-indent-shift-right start end))
+  (setq deactivate-mark nil)
+)
+
+(global-set-key (kbd "M-<up>") 'move-text-up)
+(global-set-key (kbd "M-<down>") 'move-text-down)
+
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (define-key python-mode-map (kbd "M-<right>")
+	      'balle-python-shift-right)
+	    (define-key python-mode-map (kbd "M-<left>")
+	      'balle-python-shift-left))
+	  )
+
 (provide 'epy-editing)
