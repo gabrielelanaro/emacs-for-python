3924aee6cdde24db82fecc1227bd4f0467bb852e
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2011-10-26 20:21:03 +0200

    Added ipython integration

--- epy-python.el
+++ epy-python.el
@@ -38,7 +38,18 @@
                     )))
   )
 
-
+;; Ipython integration with fgallina/python.el
+(defun epy-setup-ipython ()
+  "Setup ipython integration with python-mode"
+  (interactive)
+  (setq
+   python-shell-interpreter "ipython"
+   python-shell-interpreter-args ""
+   python-shell-prompt-regexp "In \[[0-9]+\]: "
+   python-shell-prompt-output-regexp "Out\[[0-9]+\]: "
+   python-shell-completion-setup-code ""
+   python-shell-completion-string-code "';'.join(__IP.complete('''%s'''))\n")
+  )
 
 ;;=========================================================
 ;; Flymake additions, I have to put this one somwhere else?
