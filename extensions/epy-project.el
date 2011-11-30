d924a7e4011becb763e0a08c6e7863b64cc377f9
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2011-11-30 20:49:04 +0100

    project management start devel

---
+++ extensions/epy-project.el
@@ -0,0 +1,10 @@
+;; Project-related founctions for emacs-for-python
+
+(defun epy-proj-build-menu ()
+  "Build the menu for epy-proj"
+  )
+
+(defun epy-proj-build-test-menu ()
+  "Build the sub-menu related to test discovery"
+  )
+
