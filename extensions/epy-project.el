cee4d3c7e75a6d0342f657a89e9967cb13f62559
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2011-11-30 22:19:54 +0100

    how much things we learn, we have created the dumb menu with dummy elements

--- extensions/epy-project.el
+++ extensions/epy-project.el
@@ -2,9 +2,24 @@
 
 (defun epy-proj-build-menu ()
   "Build the menu for epy-proj"
+  (define-key-after global-map [menu-bar pyproject]
+    (cons "Epy"(make-sparse-keymap "Epy"))
+    'tools)
   )
 
 (defun epy-proj-build-test-menu ()
   "Build the sub-menu related to test discovery"
+  
+  (define-key global-map [menu-bar pyproject tests]
+    (cons "Tests" (make-sparse-keymap "Tests"))
+    )
+  
+  (let ((tests '("hello.ok" "hello.no" "hello.maybe")))
+    (dolist (test tests)
+      (define-key global-map (vector 'menu-bar 'pyproject 'tests (make-symbol test))
+	(cons test (make-sparse-keymap test)))
+      )
+    )
   )
+
 
