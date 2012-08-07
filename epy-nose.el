bb4ea6d5dfba3b9514587280ae35b3697c1a8062
Ben Ford <bford@solaisecapital.com>
2012-08-07 15:24:36 +0100

    Add a command for running nose until the first failing test (nosetests -x)

--- epy-nose.el
+++ epy-nose.el
@@ -2,14 +2,15 @@
 
 ;; Nose bindings
 (add-hook 'python-mode-hook
-	  (lambda ()
-	    (local-set-key "\C-ca" 'nosetests-all)
-	    (local-set-key "\C-cM" 'nosetests-module) ;; C-c m conflicts w/ pylint
-	    (local-set-key "\C-c." 'nosetests-one)
-	    (local-set-key "\C-cpa" 'nosetests-pdb-all)
-	    (local-set-key "\C-cpm" 'nosetests-pdb-module)
-	    (local-set-key "\C-cp." 'nosetests-pdb-one))
-	  )
+          (lambda ()
+            (local-set-key "\C-ca" 'nosetests-all)
+            (local-set-key "\C-cM" 'nosetests-module)  ;; C-c m conflicts w/ pylint
+            (local-set-key "\C-c." 'nosetests-one)
+            (local-set-key "\C-cx" 'nosetests-stop)
+            (local-set-key "\C-cpa" 'nosetests-pdb-all)
+            (local-set-key "\C-cpm" 'nosetests-pdb-module)
+            (local-set-key "\C-cp." 'nosetests-pdb-one))
+          )
 
 
 (provide 'epy-nose)
