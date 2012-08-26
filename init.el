be8da09f41d865d7676057d69f88289d5c846daf
Evgeny Cherkashin <eugene@irnok.net>
2012-08-26 13:48:29 +0900

    Switched to swi prolog system.

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
 (autoload 'run-prolog "prolog" "Start a Prolog sub-process." t)
 (autoload 'prolog-mode "prolog" "Major mode for editing Prolog programs." t)
 (autoload 'mercury-mode "prolog" "Major mode for editing Mercury programs." t)
-(setq prolog-system 'gnu)
+(setq prolog-system 'swi) ;; gnu
 (setq auto-mode-alist (append '(("\\.pl$" . prolog-mode)
                                 ("\\.m$" . mercury-mode))
                                auto-mode-alist))
