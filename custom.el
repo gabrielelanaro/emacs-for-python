c1e99f90dcac3bd6cd553ad1ac0bb8171f41839d
Evgeny Cherkashin <eugene@irnok.net>
2012-09-30 22:20:37 +0900

    Get our custom.el together.

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((py-master-file . "/path/to/interactivetest.py") (whitespace-line-column . 80) (lexical-binding . t))))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "grey28" :foreground "wheat1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 159 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
+ '(linum ((t (:inherit (shadow default) :background "black" :foreground "orange"))))
+ '(region ((((class color) (min-colors 88) (background dark)) (:background "orange3"))))
+ '(tooltip ((((class color)) (:inherit variable-pitch :background "lightyellow" :foreground "black" :height 0.5)))))
