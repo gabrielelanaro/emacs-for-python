db79c52da7bb22c79704222eacf50272a1b30747
Evgeny Cherkashin <eugene@irnok.net>
2012-10-04 03:10:32 +0900

    Windows-setup

--- custom.el
+++ custom.el
@@ -12,7 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "grey28" :foreground "wheat1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 159 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "grey28" :foreground "wheat1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "outline" :family "Lucida Sans"))))
  '(flyspell-duplicate ((t (:foreground "pale green"))))
  '(flyspell-incorrect ((t (:foreground "orange red"))))
  '(highlight-changes ((t (:foreground "LightGoldenrod1"))))
