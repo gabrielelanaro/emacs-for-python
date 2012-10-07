f1122f9fc5d9d89beb2cfb2760cc8805f5513472
Evgeny Cherkashin <eugene@irnok.net>
2012-10-07 10:18:22 +0900

    Packed my color scheme into a theme.

--- custom.el
+++ custom.el
@@ -1,23 +1,13 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(current-language-environment "Russian")
- '(safe-local-variable-values (quote ((TeX-master . "dis") (py-master-file . "/path/to/interactivetest.py") (whitespace-line-column . 80) (lexical-binding . t))))
- '(show-paren-mode t)
- '(tool-bar-mode nil))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (eugeneai-theme)))
+ '(custom-safe-themes (quote ("d5af8f6ee92912a7bac8185ed447e1e825252728a1ebd1d41eb2da660d27ad62" default))))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "grey28" :foreground "wheat1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 101 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
- '(flyspell-duplicate ((t (:foreground "pale green"))))
- '(flyspell-incorrect ((t (:foreground "orange red"))))
- '(highlight-changes ((t (:foreground "LightGoldenrod1"))))
- '(highlight-changes-delete ((t (:foreground "dark orange" :strike-through t))))
- '(linum ((t (:inherit (shadow default) :background "black" :foreground "orange"))))
- '(region ((((class color) (min-colors 88) (background dark)) (:background "orange3"))))
- '(tex-verbatim ((t (:family "Ubuntu Mono"))))
- '(tooltip ((((class color)) (:inherit variable-pitch :background "lightyellow" :foreground "black" :height 0.5)))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
