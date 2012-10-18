545bd0c987c272f38dfd74a2e4e435fc9c21dfa5
Evgeny Cherkashin <eugeneai@irnok.net>
2012-10-18 10:30:23 +0900

    Commented out aspell default dictionary setup as it differs for different systems.

--- init.el
+++ init.el
@@ -540,6 +540,6 @@
   )
 
 (setq-default ispell-program-name "aspell")
-(setq ispell-dictionary "english")
-(setq ispell-local-dictionary "russian")
-(setq flyspell-default-dictionary "russian")
+;(setq ispell-dictionary "english")
+;(setq ispell-local-dictionary "russian")
+;(setq flyspell-default-dictionary "russian")
