be9c4f2888fe2f142042d045171cd75a1523a9fb
Evgeny Cherkashin <eugene@irnok.net>
2013-06-18 21:25:38 +0900

    Added Vala support.

--- init.el
+++ init.el
@@ -88,6 +88,12 @@
 (setq auto-mode-alist
    (cons '("\.md" . markdown-mode) auto-mode-alist))
 
+(autoload 'vala-mode "vala-mode" "Major mode for editing Vala code." t)
+(add-to-list 'auto-mode-alist '("\.vala$" . vala-mode))
+(add-to-list 'auto-mode-alist '("\.vapi$" . vala-mode))
+(add-to-list 'file-coding-system-alist '("\.vala$" . utf-8))
+(add-to-list 'file-coding-system-alist '("\.vapi$" . utf-8))
+
 ;;Setting up tabbar
 (if
     windowed-system
