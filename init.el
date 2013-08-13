0bb98031c99bda02ce0609a2b0f55fa1b1d95e4b
Evgeny Cherkashin <eugene@irnok.net>
2013-08-13 10:43:28 +0900

    Corrected wrong comment mark.

--- init.el
+++ init.el
@@ -56,7 +56,7 @@
 (if win32-system
     (setenv "PYMACS_PYTHON" "c:/python27/python.exe")
     (setenv "PYMACS_PYTHON" "python2")
-    # (setenv "PYMACS_PYTHON" "/usr/bin/python2")
+    ;; (setenv "PYMACS_PYTHON" "/usr/bin/python2")
 )
 (load-file (expand-file-name "epy-init.el" dotfiles-dir))
 
