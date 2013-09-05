40311764c4104b9c7cc17229b7392d232945383e
Evgeny Cherkashin <eugene@irnok.net>
2013-09-05 19:38:32 +0900

    Removed CEDET dependability.

--- custom.el
+++ custom.el
@@ -5,8 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (eugeneai-theme)))
  '(custom-safe-themes (quote ("d5af8f6ee92912a7bac8185ed447e1e825252728a1ebd1d41eb2da660d27ad62" default)))
- '(ispell-dictionary "ru-yeyo")
- '(text-mode-hook (quote (turn-on-flyspell text-mode-hook-identify))))
+ '(ispell-dictionary "ru-yeyo" t)
+ '(text-mode-hook (quote (turn-on-flyspell text-mode-hook-identify)))
+ '(which-function-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -36,11 +36,9 @@
 (if win32-system
     (progn
       (setq custom-file (expand-file-name "custom-w32.el" dotfiles-dir))
-      (load custom-file)
       )
     (progn
       (setq custom-file (expand-file-name "custom.el" dotfiles-dir))
-      (load custom-file)
       )
     )
 
@@ -247,6 +245,8 @@
 (add-to-list 'auto-mode-alist '("\\.vapi$" . vala-mode))
 (add-to-list 'file-coding-system-alist '("\\.vala$" . utf-8))
 (add-to-list 'file-coding-system-alist '("\\.vapi$" . utf-8))
+
+(load custom-file)
 
 (if
     windowed-system
@@ -558,45 +558,9 @@
 (unless (server-running-p) (server-start))
 
 
-;; Load CEDET.
-;; See cedet/common/cedet.info for configuration details.
-;; IMPORTANT: For Emacs >= 23.2, you must place this *before* any
-;; CEDET component (including EIEIO) gets activated by another
-;; package (Gnus, auth-source, ...).
-(load-file "~/.emacs.d/site-lisp/cedet-1.1/common/cedet.el")
-;;.emacs.d/site-lisp/cedet-1.1
-;; Enable EDE (Project Management) features
-(global-ede-mode 1)
-
-;; Enable EDE for a pre-existing C++ project
-;; (ede-cpp-root-project "NAME" :file "~/myproject/Makefile")
-
-
-;; Enabling Semantic (code-parsing, smart completion) features
-;; Select one of the following:
-
-;; * This enables the database and idle reparse engines
-(semantic-load-enable-minimum-features)
-
-;; * This enables some tools useful for coding, such as summary mode,
-;;   imenu support, and the semantic navigator
-(semantic-load-enable-code-helpers)
-
-;; * This enables even more coding tools such as intellisense mode,
-;;   decoration mode, and stickyfunc mode (plus regular code helpers)
-(semantic-load-enable-gaudy-code-helpers) ;;
-
-;; * This enables the use of Exuberant ctags if you have it installed.
-;;   If you use C++ templates or boost, you should NOT enable it.
-(semantic-load-enable-all-exuberent-ctags-support) ;;
-;;   Or, use one of these two types of support.
-;;   Add support for new languages only via ctags.
-;; (semantic-load-enable-primary-exuberent-ctags-support)
-;;   Add support for using ctags as a backup parser.
-;; (semantic-load-enable-secondary-exuberent-ctags-support)
-
-;; Enable SRecode (Template management) minor-mode.
-(global-srecode-minor-mode 1) ;;
-
-(add-to-list 'load-path "~/.emacs.d/site-lisp/jdee-2.4.1/lisp")
-(load "jde")
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+;(unless (package-installed-p 'scala-mode2)
+;  (package-refresh-contents) (package-install 'scala-mode2))
