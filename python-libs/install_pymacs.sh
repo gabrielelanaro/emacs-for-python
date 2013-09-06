a27d5a5748e3a82bfeddcd3062e1dcbc866eb087
Evgeny Cherkashin <eugeneai@irnok.net>
2013-09-06 13:53:57 +0900

    Convert static directory of python libraries (Pymacs) to an installer script based subsystem.

--- python-libs/install_pymacs.sh
+++ python-libs/install_pymacs.sh
@@ -1,1 +1,26 @@
-PYTHONPATH=./ easy_install-2.7 -d ./ rope ropemacs pymacs
+#!/bin/sh
+
+PYMACS_VERSION=0.25
+PYTHON_VERSION=2.7
+
+PYTHON=python$PYTHON_VERSION
+EI=easy_install-$PYTHON_VERSION
+
+#cleanup
+rm -rf Pymacs* rope* site.* easy-install*
+
+PYTHONPATH=./ $EI -z -d ./ rope ropemacs 
+wget https://github.com/pinard/Pymacs/archive/master.tar.gz -O - | tar xzvf -
+
+DNAME=$(ls -d *Pymacs*)
+pushd $DNAME
+$PYTHON pppp -C ppppconfig.py *.in contrib tests
+$PYTHON setup.py sdist
+PYMACS_VERSION=$(python -c \"import Pymacs;print (Pymacs.version)\")
+echo "Pymacs-version: $PYMACS_VERSION"
+cp dist/Pymacs-$PYMACS_VERSION.tar.gz ../
+cp -i pymacs.el ../../extensions/
+popd
+rm -rf $DNAME
+PYTHONPATH=./ $EI -z -d ./ Pymacs-$PYMACS_VERSION.tar.gz
+rm -f Pymacs-$PYMACS_VERSION.tar.gz
