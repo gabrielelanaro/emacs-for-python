9872904af0ebae9773c76262b5c043ab68e09ef1
Evgeny Cherkashin <eugeneai@irnok.net>
2013-09-06 13:51:43 +0900

    Remove absolute path.

--- init.el
+++ init.el
@@ -54,7 +54,6 @@
 (if win32-system
     (setenv "PYMACS_PYTHON" "c:/python27/python.exe")
     (setenv "PYMACS_PYTHON" "python2")
-    ;; (setenv "PYMACS_PYTHON" "/usr/bin/python2")
 )
 (load-file (expand-file-name "epy-init.el" dotfiles-dir))
 
