40eb78388e5f147464f380a7337e611005060101
Evgeny Cherkashin <eugene@irnok.net>
2013-09-12 22:06:59 +0900

    Merge with upstream

--- custom.el
+++ custom.el
@@ -6,6 +6,7 @@
  '(custom-enabled-themes (quote (eugeneai-theme)))
  '(custom-safe-themes (quote ("d5af8f6ee92912a7bac8185ed447e1e825252728a1ebd1d41eb2da660d27ad62" default)))
  '(ispell-dictionary "ru-yeyo" t)
+ '(safe-local-variable-values (quote ((py-indent-offset . 4) (TeX-master . "dis") (py-master-file . "/path/to/interactivetest.py") (whitespace-line-column . 80) (lexical-binding . t))))
  '(text-mode-hook (quote (turn-on-flyspell text-mode-hook-identify)))
  '(which-function-mode nil))
 (custom-set-faces
