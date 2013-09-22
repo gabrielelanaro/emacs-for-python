c2d0225fd55a94bb55a30bf12ee23d1f1c996899
Evgeny Cherkashin <eugene@irnok.net>
2013-09-22 15:11:03 +0900

    Added D-mode.

---
+++ elpa/d-mode-20130811.22/d-mode-autoloads.el
@@ -0,0 +1,35 @@
+;;; d-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (d-mode) "d-mode" "d-mode.el" (21054 35220 593912
+;;;;;;  596000))
+;;; Generated autoloads from d-mode.el
+ (add-to-list 'auto-mode-alist '("\\.d[i]?\\'" . d-mode))
+
+(autoload 'd-mode "d-mode" "\
+Major mode for editing code written in the D Programming Language.
+See http://www.digitalmars.com/d for more information about the D language.
+The hook `c-mode-common-hook' is run with no args at mode
+initialization, then `d-mode-hook'.
+
+Key bindings:
+\\{d-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("d-mode-pkg.el") (21054 35220 703794 379000))
+
+;;;***
+
+(provide 'd-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; d-mode-autoloads.el ends here
---
+++ elpa/d-mode-20130811.22/d-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "d-mode" "20130811.22" "D Programming Language mode for (X)Emacs [github]" (quote nil))
