f4ab13f4ae1b110106278312f4ed0a2cffeb975d
Evgeny Cherkashin <eugeneai@irnok.net>
2013-10-06 15:55:14 +0900

    Local option's changes.

--- custom.el
+++ custom.el
@@ -11,4 +11,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Fira Mono OT" :foundry "unknown" :slant normal :weight normal :height 120 :width normal)))))
