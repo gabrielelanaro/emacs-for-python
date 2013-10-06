bf62e1f46cdbf116630439e605f43fd3381a06a2
Evgeny Cherkashin <eugene@irnok.net>
2013-10-06 15:53:17 +0900

    Local options's changes.

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
  '(column-number-mode t)
  '(custom-enabled-themes (quote (deeper-blue)))
  '(custom-safe-themes (quote ("e200f481e31ceee929079ad8a6f629e45bdad45e5ac27101c89d55f4827071db" "e885ba299f1f0f0927bc8b10136a704c1ec05c7d04b7011559439dd9e56c56ab" "6252dbc43eefee0edc82ef000659ece4f77c4c989b4571d8d23485efc5e9ef4d" "d5af8f6ee92912a7bac8185ed447e1e825252728a1ebd1d41eb2da660d27ad62" default)))
- '(ispell-dictionary "ru-yeyo" t)
+ '(ispell-dictionary "ru-yeyo")
  '(minibuffer-auto-raise t)
  '(minibuffer-frame-alist (quote ((width . 80) (height . 1))))
  '(safe-local-variable-values (quote ((TeX-master . t) (ispell-dictionary . american) (py-indent-offset . 4) (TeX-master . "dis") (py-master-file . "/path/to/interactivetest.py") (whitespace-line-column . 80) (lexical-binding . t))))
@@ -22,7 +22,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Inconsolata LGC" :foundry "unknown" :slant normal :weight normal :height 101 :width normal))))
+ '(default ((t (:inherit nil :stipple nil :background "#181a26" :foreground "gray80" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 101 :width normal :foundry "unknown" :family "Fira Mono"))))
  '(minibuffer-prompt ((t (:foreground "CadetBlue1"))))
  '(mode-line ((t (:background "gray75" :foreground "black" :weight normal :height 0.5 :family "Droid Sans"))))
- '(mode-line-highlight ((t (:background "gold")))))
+ '(mode-line-highlight ((t (:background "gold"))))
+ '(tex-verbatim ((t (:family "Inconsolata LGC Medium")))))
