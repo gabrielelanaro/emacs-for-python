25b1000e2c501db5a0fcfc119604745c33ffbbd4
Evgeny Cherkashin <eugeneai@irnok.net>
2014-03-12 16:44:52 +0900

    Added goto-last-change module.

--- init.el
+++ init.el
@@ -731,3 +731,6 @@ ov)
  'compilation-error-regexp-alist
  '("^\\([^ \n]+\\)(\\([0-9]+\\)): \\(?:error\\|.\\|warnin\\(g\\)\\|remar\\(k\\)\\)"
    1 2 nil (3 . 4)))
+
+(require 'goto-last-change)
+(global-set-key (kbd "C-x C-\\") 'goto-last-change)
