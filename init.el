b9e191adea00f71eb61051df940321dfdcb3f898
Evgeny Cherkashin <eugeneai@irnok.net>
2014-03-28 16:51:48 +0900

    Turn on jump char again.

--- init.el
+++ init.el
@@ -469,10 +469,10 @@
                           global-proc-buffer-name))))
 
 
-;(require 'jump-char)
+(require 'jump-char)
 
-;(global-set-key [(meta m)] 'jump-char-forward)
-;(global-set-key [(shift meta m)] 'jump-char-backward)
+(global-set-key [(meta m)] 'jump-char-forward)
+(global-set-key [(shift meta m)] 'jump-char-backward)
 
 (defun set-input-method-english ()
   (interactive)
