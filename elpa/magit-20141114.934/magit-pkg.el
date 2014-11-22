28a2e0b24865f91dc4da3ed633e9851205a4376b
Evgeny Cherkashin <eugene@irnok.net>
2014-11-22 08:36:30 +0800

    Local chabges.

---
+++ elpa/magit-20141114.934/magit-pkg.el
@@ -0,0 +1,9 @@
+(define-package "magit" "20141114.934" "control Git from Emacs"
+  '((cl-lib "0.3")
+    (git-commit-mode "0.14.0")
+    (git-rebase-mode "0.14.0"))
+  :keywords
+  '("vc" "tools"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
